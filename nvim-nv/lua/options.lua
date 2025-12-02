7245ae4f82e5f15b7411adfaf15184364a2a2f5c
haarit19058 <haarit1905@gmail.com>
2025-12-02 23:45:03 +0530

    updated nv

--- nvim-nv/lua/options.lua
+++ nvim-nv/lua/options.lua
@@ -1,5 +1,15 @@
 require "nvchad.options"
 
+
+local opt = vim.opt
+
+opt.tabstop = 4      -- number of spaces a <Tab> counts for
+opt.shiftwidth = 4   -- number of spaces for autoindent
+opt.expandtab = true -- convert tabs to spaces
+opt.softtabstop = 4  -- backspace treats indent as 4 spaces
+
+
+
 -- add yours here!
 
 -- local o = vim.o
